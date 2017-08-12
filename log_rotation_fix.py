c867918f22a99993782d382a0a306d40dfabf93f
Bartosz Zurkowski <zurkowski.bartosz@gmail.com>
2017-08-13 01:37:23 +0200

    Prepare rotation fix for multiple parts

--- log_rotation_fix.py
+++ log_rotation_fix.py
@@ -3,16 +3,19 @@ import os, sys, re
 CONSOLE_REGEX = re.compile('\Aconsole\.log\.\d+\Z')
 
 
-for root, _dirs, filenames in os.walk(sys.argv[1]):
-    part_names = sorted([fn for fn in filenames if CONSOLE_REGEX.match(fn)])
-    if not any(part_names):
+for root, _dirs, fnames in os.walk(sys.argv[1]):
+    part_names = sorted([fn for fn in fnames if CONSOLE_REGEX.match(fn)])
+    if not part_names:
         continue
-    dst_path = os.path.join(root, 'console.log')
-    print root, dst_path
-    with open(dst_path, 'a') as dst_file:
+    part_names = list(reversed(part_names))
+    part_names.append('console.log')
+
+    dest_path = os.path.join(root, 'console.log.tmp')
+    with open(dest_path, 'a') as dest:
         for part_name in part_names:
             part_path = os.path.join(root, part_name)
-            with open(part_path, 'r') as part_file:
-                entries = part_file.read()
-            dst_file.write(entries)
+            with open(part_path, 'r') as part:
+                data = part.read()
+            dest.write(data)
             os.remove(part_path)
+    os.rename(dest_path, dest_path[:-4])
